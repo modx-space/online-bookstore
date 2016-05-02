2dc97c4a55a1ea84a6c19db7c229c7725b0ff78d
shawzt <shawzt@gmail.com>
2016-05-02 17:26:00 +0800

    apply Devise to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-
+  # devise modules:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :trackable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  # devise_for :users
+  devise_for :users
+  
   get 'contact' => 'home#contact'
 
   resources :books do
