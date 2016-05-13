9be4590a1e25acaa69c2a2996ed4059023dd7769
shawzt <shawzt@gmail.com>
2016-05-13 16:32:56 +0800

    enhance the logic of checking whether user is ordering or borrowing the book to avoid duplication

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   has_many :borrows
 
   def borrow(book)
-    if is_borrowing?(book)
-      record = Borrow.find_by book: book, user: self, status: Borrow::BORROWING
+    if is_ordering_or_borrowing?(book)
+      record = Borrow.find_by book: book, user: self, status: [Borrow::PENDING, Borrow::BORROWING]
       is_new_record = false
     else
       record = self.borrows.create(book: book, should_return_date: 2.weeks.from_now, status: Borrow::BORROWING)
@@ -21,7 +21,11 @@ class User < ActiveRecord::Base
   end
 
   private
-  def is_borrowing?(book)
-    self.borrows.to_ary.collect { |borrow| borrow.book_id if borrow.status == Borrow::BORROWING }.include?(book.id)
+  def is_ordering_or_borrowing?(book)
+    book_ids = self.borrows.to_ary.collect do |borrow|
+      borrow.book_id if [Borrow::PENDING, Borrow::BORROWING].include?(borrow.status)
+    end
+
+    book_ids.include?(book.id)
   end
 end
