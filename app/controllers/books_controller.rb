cb152d70b99a9c35882a2c2e6d3d15043eba9d75
shawzt <shawzt@gmail.com>
2016-05-17 15:40:29 +0800

    show borrowing and ordering information on book detail page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :find_book, :check_ordering_and_borrowing, except: [:guide, :index]
+
   def guide
   end
 
@@ -7,20 +9,27 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find params[:id]
   end
 
   def borrow
-    @book = Book.find params[:id]
     @record, @is_new_record = current_user.borrow @book
 
     render :show
   end
 
   def order
-    @book = Book.find params[:id]
     @record, @is_new_record = current_user.order @book
 
     render :show
+  end
+
+  private
+  def find_book
+    @book = Book.find params[:id]
+  end
+
+  def check_ordering_and_borrowing
+    @who_is_borrowing = @book.who_is_borrowing
+    @who_is_ordering  = @book.who_is_ordering
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ActiveRecord::Base
   paginates_per 12
+
+  has_many :borrows
+
+  def who_is_borrowing
+    self.borrows.collect { |borrow| borrow.user if borrow.status == Borrow::BORROWING }.compact
+  end
+
+  def who_is_ordering
+    self.borrows.collect { |borrow| borrow.user if borrow.status == Borrow::PENDING }.compact
+  end
 end
