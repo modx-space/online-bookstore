8d6d3b998c9d3c78766ec9f9f74645d7a89a52af
shawzt <shawzt@gmail.com>
2016-05-21 18:19:05 +0800

    redirect to books guide page when user is signed in and visiting  home index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!
 
   def index
+    redirect_to guide_books_path if user_signed_in?
     @user = User.new
   end
 end
